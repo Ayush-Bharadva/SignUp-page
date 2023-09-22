f652a848e4a9d8060a9f722a71007ec8871d2e81
Ayush-Bharadva <ayushkumar.bharadva@zuru.com>
2023-09-22 14:11:24 +0530

    UI complete

--- commom/index.js
+++ commom/index.js
@@ -1,12 +1,10 @@
 /* Navigation between pages */
 const signupBtn = document.querySelector(".signup");
-// const signUpLink = document.querySelector(".text .signup-link");
 const loginBtn = document.querySelector(".login");
-// const loginLink = document.querySelector(".text .login-link");
 
 // go to signUp page
 signupBtn.addEventListener("click", () => {
-	window.location.assign("../SignUp/signup.html");
+	window.location.assign("../index.html");
 });
 //go to login page
 loginBtn.addEventListener("click", () => {
