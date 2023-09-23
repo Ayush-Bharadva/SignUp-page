121a1d02ab1880dd5ccd9e0d7aefd7666f4aa87b
Ayush Bharadva <ayushbharadva@gmail.com>
2023-09-23 16:01:02 +0530

    credentials local storage complete

--- index.js
+++ index.js
@@ -1,24 +1,59 @@
+console.log("start...");
 /* Navigation between pages */
 
-const signupBtn = document.querySelector(".signup");
-const loginBtn = document.querySelector(".login");
+setTimeout(() => {
+    // Navigate to the new page after a delay of 1 second
+    window.location.assign("./index.html");
+    console.log("end...");
+}, 1000);
 
-// go to signUp page
-signupBtn.addEventListener("click", () => {
-	window.location.assign("./index.html");
-});
-//go to login page
-loginBtn.addEventListener("click", () => {
-	window.location.assign("./login.html");
+// const signupBtn = document.querySelector(".signup");
+// const loginBtn = document.querySelector(".login");
+
+// // go to signUp page
+// signupBtn.addEventListener("click", () => {
+//     window.location.assign("./index.html");
+// });
+// //go to login page
+// loginBtn.addEventListener("click", () => {
+//     window.location.assign("./login.html");
+// });
+
+// all Existing Users
+let existingUsers = [];
+// UserData in JSON
+let storedUserDataJSON = localStorage.getItem("allUserInfo");
+
+if (storedUserDataJSON) {
+    existingUsers = JSON.parse(storedUserDataJSON);
+}
+
+console.log("allusers :", existingUsers);
+
+const signUpForm = document.querySelector("#signup-form");
+
+signUpForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // input credentials
+    const username = document.querySelector("#username").value;
+    const email = document.querySelector("#email").value;
+    const pass = document.querySelector("#pass").value;
+    const confirmPass = document.querySelector("#confirm-pass").value;
+
+    // getting previous users
+    let prevUsers = JSON.parse(localStorage.getItem("allUserInfo")) || [];
+
+    // combining current + previous users
+    prevUsers.push({ username, email, pass, confirmPass });
+
+    // storing user credentials in local storage
+    localStorage.setItem("allUserInfo", JSON.stringify(prevUsers));
+
+    console.log("user added...");
+
+    // clearing form
+    signUpForm.reset();
 });
 
-const submitLogin = document.querySelector(".submit-login");
-const submitSignup = document.querySelector(".submit-signup");
-console.log("submitLogin :", submitLogin);
-console.log("submitSignup :", submitSignup);
-submitSignup.addEventListener("click", function (e) {
-	console.log("submit-signup target:", e.target);
-});
-submitLogin.addEventListener("click", (e) => {
-	console.log("submit-login target:", e.target);
-});
+console.log("end...");
