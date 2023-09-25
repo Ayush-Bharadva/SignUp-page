a893dbf1be9a267f1372cd0d7aaeee546e0d94dd
Ayush-Bharadva <ayushkumar.bharadva@zuru.com>
2023-09-25 13:04:42 +0530

    login page completed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-/* form submit operation */
+/* for signup form operation */
 const signUpForm = document.querySelector("#signup-form");
 const username = document.querySelector("#username");
 const email = document.querySelector("#email");
 const password = document.querySelector("#pass");
 const confirmPass = document.querySelector("#confirm-pass");
 const signupBtn = document.querySelector(".signup");
-
+// TODO change variable names, username -> userameValue, userameValue -> username(similarly for all)
 let userCredentials = {};
 // all Existing Users
 let existingUsers = [];
@@ -18,34 +18,37 @@ if (storedUserDataJSON) {
 
 let successfulSignup = false; // Flag to indicate successful signup
 
-signUpForm.addEventListener("submit", (event) => {
-	event.preventDefault();
-	userCredentials = {
-		usernameValue: username.value,
-		emailValue: email.value,
-		passwordValue: password.value,
-		confirmPassValue: confirmPass.value,
-	};
-	let isUserSignedup = false;
+if (signUpForm) {
+	signUpForm.addEventListener("submit", (event) => {
+		event.preventDefault();
+		userCredentials = {
+			usernameValue: username.value,
+			emailValue: email.value,
+			passwordValue: password.value,
+			confirmPassValue: confirmPass.value,
+		};
+		let isUserSignedup = false;
 
-	// if true add user
-	if (validateUser(userCredentials)) {
-		let prevUsers = JSON.parse(localStorage.getItem("allUserInfo")) || [];
+		// if true add user
+		if (validateUser(userCredentials)) {
+			let prevUsers =
+				JSON.parse(localStorage.getItem("allUserInfo")) || [];
 
-		// combining current + previous users
-		prevUsers.push(userCredentials);
+			// combining current + previous users
+			prevUsers.push(userCredentials);
 
-		// storing user credentials in local storage
-		localStorage.setItem("allUserInfo", JSON.stringify(prevUsers));
-		signUpForm.reset();
-		isUserSignedup = true;
-		if (isUserSignedup) {
-			window.location.href = "./login.html";
+			// storing user credentials in local storage
+			localStorage.setItem("allUserInfo", JSON.stringify(prevUsers));
+			signUpForm.reset();
+			isUserSignedup = true;
+			if (isUserSignedup) {
+				window.location.href = "./login.html";
+			}
+		} else {
+			console.log("false user credentials..");
 		}
-	} else {
-		console.log("false user credentials..");
-	}
-});
+	});
+}
 
 // function to validate user
 function validateUser(user) {
@@ -104,10 +107,86 @@ function setInputError(input, message) {
 	const small = input.nextElementSibling;
 	small.classList.add("error");
 	small.innerText = message;
-	console.log(small, message);
+	// console.log(small, message);
 }
 function setInputSuccess(input) {
 	const field = input;
 	input.style.border = "1.5px solid green";
 	input.nextElementSibling.classList.remove("error");
+}
+
+// for login form operations
+const loginEmail = document.querySelector(".login-email");
+const loginPass = document.querySelector(".login-password");
+const loginForm = document.querySelector("#login-form");
+
+let loginCredentials = {};
+
+if (loginForm) {
+	loginForm.addEventListener("submit", (event) => {
+		event.preventDefault();
+		loginCredentials = {
+			emailValue: loginEmail.value,
+			passwordValue: loginPass.value,
+		};
+		let isLoggedin = false;
+		if (validateLogin(loginCredentials)) {
+			loginForm.reset();
+			isLoggedin = true;
+			if (isLoggedin) {
+				window.location.href = "./home.html";
+			}
+			console.log("logged in successfully");
+		} else {
+			console.log("didnot logged in");
+		}
+	});
+}
+
+function validateLogin(user) {
+	let { emailValue, passwordValue } = user;
+
+	let isValidCredential = true;
+
+	// for email
+	if (emailValue === "") {
+		isValidCredential = false;
+		console.log("email cannot be blank");
+		setInputError(loginEmail, "email cannot be blank");
+	} else if (
+		existingUsers.every((existingUser) => {
+			return existingUser.emailValue !== emailValue;
+		})
+	) {
+		isValidCredential = false;
+		console.log("email does not exist");
+		setInputError(loginEmail, "email does not exist");
+	} else {
+		isValidCredential = true;
+		console.log("login email exists");
+		setInputSuccess(loginEmail);
+	}
+
+	// for password
+	isValiduser = existingUsers.some((existingUser) => {
+		return (
+			existingUser.emailValue === emailValue &&
+			existingUser.passwordValue === passwordValue
+		);
+	});
+
+	if (passwordValue === "") {
+		isValidCredential = false;
+		console.log("password cannot be blank");
+		setInputError(loginPass, "password cannot be blank");
+	} else if (!isValiduser) {
+		isValidCredential = false;
+		console.log("username and password didnt match");
+		setInputError(loginPass, "username and password didnt match");
+	} else {
+		isValidCredential = true;
+		setInputSuccess(loginPass);
+		console.log("login user credentials matched");
+	}
+	return isValidCredential;
 }
